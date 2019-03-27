23018dfb4e026f1b77536124d497e202dffb5206
Miguel Archila <miguel.4to.1994@gmail.com>
2019-03-26 22:01:42 -0600

    Segundo commit

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use(myConnection(mysql, {
     database: 'davidvela'
 }, 'single'));
 
+
 app.use(express.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
